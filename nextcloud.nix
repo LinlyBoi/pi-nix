efe00a9f3b2506bc4d56343194af0b2fd3bf8caf
Aly Sewelam <linlysmolworthy@gmail.com>
2026-09-22 08:51:34 +0300

    feat: move nextcloud to .pi domain

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   services.nextcloud = {
     enable = true;
@@ -8,10 +8,27 @@
       dbtype = "pgsql";
       adminpassFile = "/etc/nextcloud/admin-pass";
     };
+  settings = {
+    trusted_domains = [ "raspberrypi" "nextcloud.pi" ];
+    overwriteprotocol = "http"; # Use "https" if you set up TLS in Caddy later
   };
+};
+  # Configure Nextcloud's trusted domains so it accepts proxy requests from Caddy
 
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
+# Shift NGINX (Nextcloud's default backend) off port 80 onto 8080
+  services.nginx.virtualHosts."${config.services.nextcloud.hostName}" = {
+  listen = [
+    {
+      addr = "127.0.0.1";
+      port = 8080;
+    }
   ];
+};
+
+# Route nextcloud.pi from Caddy to NGINX on port 8080
+  services.caddy.virtualHosts."http://nextcloud.pi" = {
+    extraConfig = ''
+      reverse_proxy 127.0.0.1:8080
+    '';
+  };
 }
